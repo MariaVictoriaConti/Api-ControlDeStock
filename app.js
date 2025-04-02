2693b23af68268e12dcdf8e8406a78f115f76a16
victoriaConti <mavictoriaconti@gmail.com>
2025-04-02 05:45:20 -0300

    Agrego Cors + express.static

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 // Importamos express
 const express = require('express');
+const cors = require('cors');
 const dotenv = require('dotenv');
+const path = require('path')
 const routes = require('./backend/Routes/routes');
 const endpointLimiter = require('./backend/Middlewares/endpointLimiter')
+
 
 // Concetamos a la base de datos
 const connectDB = require('./backend/DataBase/dataBase')
@@ -12,10 +15,18 @@ connectDB() // aca establecemos conexion
 
 const app = express()
 app.use(express.json())
-
-//app.use(endpointLimiter);
+app.use(cors()); // Habilita CORS para todas las rutas
 
 app.use('/', routes);
+
+// Servir los archivos estaticos en el front
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/index.html'))
+// })
+
+//app.use(endpointLimiter);
 
 app.listen(process.env.PORT, () => {
     console.log(`Servidor corriendo en: http://localhost:${process.env.PORT}`);
